6a5142e4fb81d2d3c87ae8b9ecfac04562c33942
Immac <immac.gm@gmail.com>
2016-09-23 00:47:06 -0600

    move .h files to the 'include' folder

--- build.sh
+++ build.sh
@@ -1,10 +1,2 @@
-# mkdir bin -p
-# mkdir gen -p
-# mkdir build -p
-# bison --defines=tokens.h -o gen/expr_parser.cpp src/expr.y
-# mv tokens.h gen/tokens.h
-# cp src/*.h gen/
-# cp src/*.h include/
-# flex -o gen/expr_lexer.cpp src/expr.l 
 cmake .
 make
